94e04f56a5ba4bd8ea9c280116a411abf2c3a026
Steve Vitali <14stevevitali@gmail.com>
2015-05-08 20:07:03 -0400

    Fix esoteric Heroku bug

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/bower_components'));
+
 // View engine setup
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-
-app.use(express.static(__dirname + '/static'));
-app.use(express.static(__dirname + '/bower_components'));
 
 require('./routes')(app);
 
